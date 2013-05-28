8f9b0662fae76795fa880634f4dc6364c7092fac
Yu Liang <yu.liang@thekono.com>
2013-05-28 16:49:15 +0800

    split pdf into individual page

--- PDFPreprocessor/main.sikuli/main.py
+++ PDFPreprocessor/main.sikuli/main.py
@@ -2,6 +2,7 @@
 import fnmatch
 import os
 import os.path
+import shutil
 import sys
 
 cwd = os.path.dirname(getBundlePath())
@@ -17,13 +18,14 @@ reload(pdfutil)
 reload(preview)
 
 # intermediate result directories
-DIR_PREVIEW = 'preview'
+DIR_PAGE = 'page'
 DIR_SRGB = 'srgb'
 DIR_VTI = 'vti'
 DIR_TIFF = 'tiff'
 DIR_BACK = 'back'
 DIR_TEXT = 'text'
 DIR_FINAL = 'final'
+#FILE_CLIPBOARD = 'clipboard'
 
 
 def get_all_pdfs():
@@ -32,11 +34,12 @@ def get_all_pdfs():
     return filter(lambda f: fnmatch.fnmatch(f, '*.pdf'), os.listdir(cwd))
 
 
-def create_intermediate_dirs(pdf_files):
+def create_intermediate_files(pdf_files):
     """Create directories for intermediate files."""
 
-    dirs = [DIR_PREVIEW, DIR_SRGB, DIR_VTI, DIR_TIFF,
+    dirs = [DIR_PAGE, DIR_SRGB, DIR_VTI, DIR_TIFF,
             DIR_BACK, DIR_TEXT, DIR_FINAL]
+    #files = [FILE_CLIPBOARD]
     
     for pdf_file in pdf_files:
         root, ext = os.path.splitext(pdf_file)
@@ -48,32 +51,45 @@ def create_intermediate_dirs(pdf_files):
         except OSError, e:
             print 'directory (', dir, ') already exists'
 
+    #for file in files:
+    #    try:
+    #        f = open(os.path.join(cwd, file), 'w')
+    #        f.close()
+    #    except OSError, e:
+    #        print 'create file(', file, ') failed'
 
-def work_on(pdf_file):
-    """Main function."""
 
-    queue = [pdf_file]
-    output = []
+def cleanup_intermediate_files():
+    """Clean up directories for intermediate files."""
+
+    for dir in (DIR_PAGE,):
+        shutil.rmtree(os.path.join(cwd, dir))
+
     
-    while len(queue) > 0:
-        file = queue[0]
-        #preview_export(pdf_file)
-        #convert_to_srgb(pdf_file)
-        #split_layer(pdf_file)
-        
-        queue = queue[1:]
-        output.append(file)
-        
 def do_preprocess(pdf_files):
     """Main loop for each pdf file."""
 
     for pdf_file in pdf_files:
-        work_on(pdf_file)
+        
+        # 1) split a pdf file, a page a pdf
+        page_dir = os.path.join(cwd, DIR_PAGE)
+        num_pages = pdfutil.split(os.path.join(cwd, pdf_file), page_dir)
+                                  
+        for i in xrange(1, num_pages + 1):
+            page_pdf = os.path.join(page_dir, '%s.pdf' % i)
+            
+        #queue = [pdf_file]
+        #output = []
+
+        #while len(queue) > 0:
+        #    file = queue[0]
+        #    queue = queue[1:]
+        #    output.append(file)
 
 def main():
 
     pdf_files = get_all_pdfs()
-    create_intermediate_dirs(pdf_files)
+    #create_intermediate_files(pdf_files)
     do_preprocess(pdf_files)
 
        
--- PDFPreprocessor/pdfutil.sikuli/pdfutil.py
+++ PDFPreprocessor/pdfutil.sikuli/pdfutil.py
@@ -1,8 +1,12 @@
 from sikuli import *
 
 # standard library imports
+import os
+import os.path
 import re
+import shutil
 import subprocess
+import uuid
 
 # third party related imports
 
@@ -28,3 +32,108 @@ def get_num_pages(abs_filename):
             return int(line[m.start():m.end()])
 
     return 0
+
+
+def open_pdf(abs_filename):
+    """Open a pdf file by Adobe Acrobat X Pro application."""
+
+    try:
+        p = subprocess.Popen(
+                ('open', '-a', 'Adobe Acrobat Pro', abs_filename),
+                stdout=subprocess.PIPE
+            )
+    except OSError, e:
+        print unicode(e)
+        return False
+
+    return True
+
+
+def close_pdfs():
+    """Close all opened pdf."""
+
+    app = App('Adobe Acrobat Pro')
+    while app.window():
+        type('w', KeyModifier.CMD)
+        wait(1)
+
+    app.close()
+
+    
+def split(abs_filename, abs_output_dir):
+    """Split the specified pdf into multiple pdf per page."""
+
+    TIMEOUT = 5
+    
+    open_pdf(abs_filename)
+    wait("1369712063644.png", TIMEOUT)
+
+    # jump to the end of pages
+    click("1369712063644.png")
+    
+    # start extract pages  
+    click("1369711747763.png")
+    extract_dlg = "1369713588056.png"
+    wait(extract_dlg, TIMEOUT)
+
+    # set split range
+    click(Pattern("1369724920088.png").targetOffset(25,0))
+    type('a', KeyModifier.CMD)
+    type('c', KeyModifier.CMD)
+    num_pages = int(_extract_clipboard())
+    print 'number of pages:', num_pages 
+    type('1')
+    click(Pattern("1369721399716.png").targetOffset(-60,0))
+    click("1369723377211.png")
+
+    # save to a directory in desktop directory
+    wait("1369723448277.png", TIMEOUT)
+    desktop_label = exists("1369723505074.png")
+    if desktop_label is not None:
+        click(desktop_label)
+    else:
+        click("1369723779275.png")
+
+    # create a temporary directory
+    tempdir = uuid.uuid1().hex
+    click("1369723600085.png")
+    click(Pattern("1369723639913.png").targetOffset(0,27))
+    type('a', KeyModifier.CMD)
+    paste(tempdir)
+    click("1369724048617.png")
+    click("1369724174496.png")
+
+    # wait until all pdf are dumped
+    basename = os.path.basename(abs_filename)
+    base, ext = os.path.splitext(basename)
+    tempdir = os.path.expanduser(os.path.join('~', 'Desktop', tempdir))
+    last_page_pdf = os.path.join(tempdir, '%s %s.pdf' % (base, num_pages))
+    last_page_pdf = os.path.expanduser(last_page_pdf)
+    while not os.path.exists(last_page_pdf):
+        wait(1)
+
+    # move pdf files to output directory
+    for i in xrange(1, num_pages + 1):
+        shutil.move(os.path.join(tempdir, '%s %s.pdf' % (base, i)),
+                    os.path.join(abs_output_dir, '%s.pdf' % i))
+    shutil.rmtree(tempdir)
+
+    close_pdfs()
+    
+    return num_pages
+
+
+def _extract_clipboard():
+
+    cwd = os.path.dirname(getBundlePath())
+    script_path = os.path.join(cwd, 'clipboard.scpt')
+    
+    try:
+        p = subprocess.Popen(["osascript", script_path], stdout=subprocess.PIPE)
+    except OSError, e:
+        print unicode(e)
+        return None
+
+    out, err = p.communicate()
+    
+    return out
