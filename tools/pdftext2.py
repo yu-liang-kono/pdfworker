206a87021a8bf6c9c409cf4a1af9519fdc8ffdbb
Yu Liang <wildmb23@gmail.com>
2013-06-03 23:08:14 +0800

    refactor pdftext2.py

--- tools/pdftext2.py
+++ tools/pdftext2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 # standard library imports
+import argparse
 from contextlib import closing
 import os
 import os.path
+import re
 import sys
 
 sys.path.insert(
@@ -17,27 +19,95 @@ from PDFDocument import PDFDocument
 from PDFPage import PDFPage
 
 
-def main(argv):
+def create_argument_parser():
+    """Create argument parser and register parameters."""
 
-    if len(argv) != 2:
-        print 'usage: python %s PDF-File' % (argv[0])
-        exit(1)
+    parser = argparse.ArgumentParser(description="""\
+Extract text geometry information from a PDF.""")
+    parser.add_argument('--pages', type=str, default=None,
+                        help="""\
+Extract the specified pages: e.g. 1,2,3 or 1-10 or 1-10,20,30.""")
+    parser.add_argument('--pagedir', type=str, default=None,
+                        help="""\
+Output every page JSON in this directory.""")
+    parser.add_argument('--output', type=str, default=None,
+                        help="""\
+PDF document output JSON.""")
+    parser.add_argument('PDF-file')
+
+    return parser
+
+def parse_pages(user_input):
+    """Parse user's input pages."""
+
+    if user_input is None:
+        return None
+
+    pages = user_input.split(',')
+    ret = []
+
+    for p in pages:
+        if p.isdigit():
+            ret.append(int(p) - 1)
+            continue
+
+        match_obj = re.match(r'(\d+)\-(\d+)', p)
+        if match_obj is None:
+            continue
+
+        start, end = match_obj.group(1), match_obj.group(2)
+        ret.extend(range(int(start) - 1, int(end)))
+
+    return ret if len(ret) != 0 else None
+
+def create_folder(folder_name):
+    """Create a folder to contain every page JSON."""
+
+    if folder_name == '':
+        return None
 
     try:
-        os.mkdir('output')
+        os.mkdir(folder_name)
     except OSError, e:
-        pass
+        print 'The directory "%s" already exists.' % folder_name
 
-    pdf_doc = PDFDocument(argv[1])
-    for ix, p in enumerate(PDFPage.create_by_xpdf(argv[1])):
-        pdf_doc.add_page(ix, p)
-        with closing(open(os.path.join('output', '%03d.json' % (ix + 1)), 'wb')) as f:
-            f.write(p.serialize())
+    return folder_name
 
-    with closing(open('output.json', 'wb')) as f:
+def main():
+
+    arg_parser = create_argument_parser()
+    arg_dict = vars(arg_parser.parse_args())
+
+    # determine what pages to be parsed
+    page_nums = parse_pages(arg_dict['pages'])
+
+    # determine whether or not to dump JSON page by page
+    page_dir = create_folder(arg_dict.get('pagedir', '').decode('utf8'))
+
+    # get the input pdf file
+    pdf_filename = arg_dict['PDF-file'].decode('utf8')
+
+    # determine the output filename
+    output_filename = arg_dict.get('output')
+    if output_filename is None:
+        basename = os.path.basename(pdf_filename)
+        base, ext = os.path.splitext(basename)
+        output_filename = '%s.json' % base
+
+    # main logic
+    pdf_doc = PDFDocument(pdf_filename)
+    pages = PDFPage.create_by_xpdf(pdf_filename, page_nums)
+    if page_dir is not None:
+        for p in pages:
+            output_file = os.path.join(page_dir, '%03d.json' % p.page_num)
+            with closing(open(output_file, 'wb')) as f:
+                f.write(p.serialize())
+
+    # output
+    with closing(open(output_filename, 'wb')) as f:
         f.write(pdf_doc.serialize())
 
 
 if __name__ == "__main__":
 
-    main(sys.argv)
+    main()
