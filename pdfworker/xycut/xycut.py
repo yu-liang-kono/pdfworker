8564e29a36e6eeb374a37c13d9eecee65f69adab
Yu Liang <yu.liang@thekono.com>
2013-07-22 18:40:01 +0800

    update xycut.py

--- pdfworker/xycut/xycut.py
+++ pdfworker/xycut/xycut.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 # standard library imports
+from operator import attrgetter
+import os.path
+import sys
 
+sys.path.insert(
+    0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+)
 # third party related imports
 
 # local library imports
-from ..Rectangle import Rectangle, TextRectangle
+from Rectangle import Rectangle, TextRectangle
 from Interval import Interval, IntervalList
+
 
 ROW = 0
 COLUMN = 1
@@ -49,20 +56,20 @@ def build_xy_tree(bboxes, root=None):
     if root is None:
         root = BaseXYNode()
 
-    x_intervals = IntervalList(*map(lambda b: Interval(b.x, b.x + b.width),
+    x_intervals = IntervalList(*map(lambda b: Interval(b.x, b.x + b.w),
                                     bboxes))
-    y_intervals = IntervalList(*map(lambda b: Interval(b.y, b.y + b.height),
+    y_intervals = IntervalList(*map(lambda b: Interval(b.y, b.y + b.h),
                                     bboxes))
 
-    x_gaps, y_gaps = x_intervals.gaps, y_intervlas.gaps
+    x_gaps, y_gaps = x_intervals.gaps, y_intervals.gaps
     world_bbox = Rectangle(x_intervals[0].begin, y_intervals[0].begin,
                            x_intervals[-1].end - x_intervals[0].begin,
-                           y_intervals[-1].end - y_intervlas[0].begin)
+                           y_intervals[-1].end - y_intervals[0].begin)
 
     max_valley_size = 0
     max_valley = None
     max_valley_direction = None
-    for direction, gaps in {X: x_gaps, Y: y_gaps}:
+    for direction, gaps in ((X, x_gaps), (Y, y_gaps)):
         for gap in gaps:
             if gap.length > max_valley_size:
                 max_valley_size = gap.length
@@ -71,25 +78,62 @@ def build_xy_tree(bboxes, root=None):
 
     if max_valley is None:
         root.bboxes = bboxes
-        return
+        return root
 
     root.children = [BaseXYNode(), BaseXYNode()]
     bbox_group1, bbox_group2 = [], []
 
     if max_valley_direction == X:
         root.cut = Cut(Rectangle(max_valley.begin, world_bbox.y,
-                                 max_valley.length, world_bbox.height), X)
+                                 max_valley.length, world_bbox.h), X)
         for bbox in bboxes:
             group = bbox_group2 if bbox.x >= max_valley.end else bbox_group1
             group.append(bbox)
 
     else:
         root.cut = Cut(Rectangle(world_bbox.x, max_valley.begin,
-                                 world_bbox.width, max_valley.length), Y)
+                                 world_bbox.w, max_valley.length), Y)
         for bbox in bboxes:
             group = bbox_group2 if bbox.y >= max_valley.end else bbox_group1
             group.append(bbox)
 
-    for child in self.children:
-        build_xy_tree(bbox_group1, child)
+    build_xy_tree(bbox_group1, root.children[0])
+    build_xy_tree(bbox_group2, root.children[1])
 
+    return root
+
+
+def traverse(root):
+
+    if len(root.children) == 0:
+        bboxes = sorted(root.bboxes, key=attrgetter('y', 'x'))
+        for bbox in bboxes:
+            print bbox.t
+
+        print '----------------------------------'
+
+    for child in root.children:
+        traverse(child)
+
+
+if __name__ == '__main__':
+
+    import ujson
+
+    if len(sys.argv) != 2:
+        print 'usage: python %s doc.json' % sys.argv[0]
+        exit(1)
+
+    f = open(sys.argv[1], 'r')
+    data = ujson.loads(f.read())
+    f.close()
+
+    bboxes = []
+    for bbox in data['data']:
+        b = TextRectangle(bbox['x'], bbox['y'],
+                          bbox['w'], bbox['h'],
+                          bbox['t'])
+        bboxes.append(b)
+
+    root = build_xy_tree(bboxes)
+    traverse(root)
